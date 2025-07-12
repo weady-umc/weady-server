f0b5c27f9c95ccc699920c8337723123acdad596
JangwooPark <70270371+pjw81226@users.noreply.github.com>
2025-07-12 18:15:40 +0900

    [FEAT] Global / Swagger 설정, ApiResponse, ExceptionHandler 생성 및 응답 통일 (#2)
    * feat: Swagger 의존성 추가, (submodule) application.yml 수정
    
    * feat: ApiResponse 생성, 응답통일 및 간단한 Exception Handler 작성
    
    * feat: ResponseEntity 생성을 위한 ResponseEntityUtil 작성
    
    * feat: Swagger Config 설정 및 (submodule) applicaiton.yml 수정
    
    * docs: README에 ERD 추가

---
+++ src/main/java/com/weady/weady/common/apiResponse/ApiErrorResponse.java
@@ -0,0 +1,23 @@
+package com.weady.weady.common.apiResponse;
+
+import com.weady.weady.common.error.errorCode.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class ApiErrorResponse extends ApiResponse<Void> {
+
+    // 실패 시에는 생성자를 통해 code와 message를 직접 입력받습니다.
+    private ApiErrorResponse(int code, String message) {
+        super(code, message, null);
+    }
+
+    // ErrorCode Enum을 사용하는 경우 (가장 권장)
+    public static ApiErrorResponse of(ErrorCode errorCode) {
+        return new ApiErrorResponse(errorCode.getCode(), errorCode.getMessage());
+    }
+
+    // 코드를 직접 지정하는 경우
+    public static ApiErrorResponse of(int code, String message) {
+        return new ApiErrorResponse(code, message);
+    }
+}
---
+++ src/main/java/com/weady/weady/infrastructure/config/SwaggerConfig.java
@@ -0,0 +1,51 @@
+package com.weady.weady.infrastructure.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        String jwtSchemeName = "bearerAuth";
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Weady API Documentation")
+                .description("Weady 프로젝트 API 명세서입니다.")
+                .version("v0.0.1");
+    }
+
+    /*
+    * 각 API 도메인 별로 그룹화하여 Swagger UI에 표시합니다.
+    * */
+    @Bean
+    public GroupedOpenApi authApi() {
+        return GroupedOpenApi.builder()
+                .group("1. 인증 API") // Swagger UI에 표시될 그룹 이름
+                .pathsToMatch("/api/v1/auth/**") // 이 그룹에 포함될 API의 URL 패턴
+                .build();
+    }
+
+    // 추후 다른 도메인 API가 추가되면 여기에 GroupedOpenApi Bean을 추가해주세요
+}
