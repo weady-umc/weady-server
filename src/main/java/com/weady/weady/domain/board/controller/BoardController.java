0a08350b4b47ee2d83efbb808983ac5a5e81adaa
JangwooPark <70270371+pjw81226@users.noreply.github.com>
2025-07-20 04:50:12 +0900

    [FIX] Swagger / 스웨거 설정 오류 수정 (#24)
    * fix: swagger 설정 변경
    
    * fix: swagger 최신 의존성으로 변경

--- src/main/java/com/weady/weady/domain/board/controller/BoardController.java
+++ src/main/java/com/weady/weady/domain/board/controller/BoardController.java
@@ -12,12 +12,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.util.List;
 
-@RestControllerAdvice
+@RestController
 @RequiredArgsConstructor
 @Tag(name = "Board", description = "보드 관련 API")
 @RequestMapping("/api/v1/board")
