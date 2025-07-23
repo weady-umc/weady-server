c8890d9d91d7206c50535ec9eda89170caf1564a
JangwooPark <70270371+pjw81226@users.noreply.github.com>
2025-07-23 16:12:26 +0900

    [REFACTOR] Global / repository 구조 변경 (#44)
    * refactor: 1차 리펙토링
    
    * refactor: auth repository 리펙토링
    
    * refactor: auth repository, board repository 수정

--- src/main/java/com/weady/weady/domain/tags/repository/season/SeasonRepository.java
+++ src/main/java/com/weady/weady/domain/tags/repository/season/SeasonRepository.java
@@ -2,9 +2,9 @@ package com.weady.weady.domain.tags.repository.season;
 
 import com.weady.weady.domain.tags.entity.SeasonTag;
 import com.weady.weady.domain.tags.entity.WeatherTag;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface SeasonRepository {
-    Optional<SeasonTag> findById(Long id);
+public interface SeasonRepository extends JpaRepository<SeasonTag, Long> {
 }
--- src/main/java/com/weady/weady/domain/tags/repository/temperature/TemperatureRepository.java
+++ src/main/java/com/weady/weady/domain/tags/repository/temperature/TemperatureRepository.java
@@ -1,10 +1,10 @@
 package com.weady.weady.domain.tags.repository.temperature;
 
 import com.weady.weady.domain.tags.entity.TemperatureTag;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface TemperatureRepository {
-    Optional<TemperatureTag> findById(Long id);
+public interface TemperatureRepository extends JpaRepository<TemperatureTag, Long> {
 }
 
