ef91eb8a9352704d84da7fafd6a9406c0b94984b
jwpark <woojang812@gmail.com>
2025-07-23 15:43:57 +0900

    refactor: auth repository 리펙토링

--- src/main/java/com/weady/weady/domain/auth/repository/JpaRefreshTokenRepository.java
+++ src/main/java/com/weady/weady/domain/auth/repository/JpaRefreshTokenRepository.java
@@ -1,16 +1,13 @@
 package com.weady.weady.domain.auth.repository;
 
 import com.weady.weady.domain.auth.entity.RefreshToken;
-import com.weady.weady.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
-    Optional<RefreshToken> findByUser(User user);
+public interface JpaRefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUserId(Long userId);
 
     Optional<RefreshToken> findByToken(String token);
-
-    void deleteByUserId(Long userId);
 
 }
