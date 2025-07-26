8459a4f5aa3068a1f57bce1877eb3db1c34ff7f4
jwpark <woojang812@gmail.com>
2025-07-26 16:07:35 +0900

    feat: 중기예보 Entity 작성

--- src/main/java/com/weady/weady/domain/weather/entity/WeatherMidDetail.java
+++ src/main/java/com/weady/weady/domain/weather/entity/WeatherMidDetail.java
@@ -17,5 +17,14 @@ public class WeatherMidDetail extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String midTermRegCode; // 중기예보 지역 코드
+
     private Integer date; // 언제 날씨 정보인지
+
+    private Integer pop; // 강수 확률
+
+    private SkyCode skyCode; // 하늘 상태 코드 (1=맑음, 2=구름많음, 3=흐림, 4=비, 5=눈)
+
+    private Integer tmx; // 최고 기온
+    private Integer tmn; // 최저 기온
 }
