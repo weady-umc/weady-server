966aa71bd3f2cfb0fa59c0b7602ecf11a5186f6c
jwpark <woojang812@gmail.com>
2025-07-27 04:02:18 +0900

    fix: KakaoClient 일부 수정(@Value 부분 수정)

--- src/main/java/com/weady/weady/common/external/kakao/KakaoClient.java
+++ src/main/java/com/weady/weady/common/external/kakao/KakaoClient.java
@@ -1,28 +1,31 @@
 package com.weady.weady.common.external.kakao;
 
-import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
-@RequiredArgsConstructor
+@Slf4j
 public class KakaoClient {
 
-    @Value("${kakao.rest-api-key}")
-    private String kakaoApiKey;
+    private final WebClient webClient;
 
-    private final WebClient webClient = WebClient.builder()
-            .baseUrl("https://dapi.kakao.com")
-            .defaultHeader("Authorization", "KakaoAK " + kakaoApiKey)
-            .build();
+    public KakaoClient(@Value("${kakao.rest-api-key}") String kakaoApiKey) {
+        this.webClient = WebClient.builder()
+                .baseUrl("https://dapi.kakao.com")
+                .defaultHeader("Authorization", "KakaoAK " + kakaoApiKey)
+                .build();
+
+        log.info("🔑 Kakao API Key loaded: {}", kakaoApiKey);
+    }
 
     public KakaoApiResponse getRegionInfo(double longitude, double latitude) {
         return webClient.get()
                 .uri(uriBuilder -> uriBuilder
                         .path("/v2/local/geo/coord2regioncode.json")
-                        .queryParam("longitude", longitude)
-                        .queryParam("latitude", latitude)
+                        .queryParam("x", longitude)
+                        .queryParam("y", latitude)
                         .build())
                 .retrieve()
                 .bodyToMono(KakaoApiResponse.class)
