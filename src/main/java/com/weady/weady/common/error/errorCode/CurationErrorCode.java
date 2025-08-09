ca3b63076d6a857149dbd7f277f259542226fc1a
nowOne <165000086+nowOne2@users.noreply.github.com>
2025-08-09 14:14:27 +0900

    feat:curation 업로드 fix: 큐레이션 중복 스크랩 문제 해결 (#114)

--- src/main/java/com/weady/weady/common/error/errorCode/CurationErrorCode.java
+++ src/main/java/com/weady/weady/common/error/errorCode/CurationErrorCode.java
@@ -9,7 +9,8 @@ import lombok.RequiredArgsConstructor;
 public enum CurationErrorCode implements ErrorCode{
 
     CURATION_NOT_FOUND(404, "해당 큐레이션을 찾을 수 없습니다."),
-    CURATION_CATEGORY_NOT_FOUND(404,"해당 큐레이션 카테고릴ㄹ 찾을 수 없습니다.");
+    CURATION_CATEGORY_NOT_FOUND(404,"해당 큐레이션 카테고리를 찾을 수 없습니다."),
+    CURATION_ALREADY_SCRAPPED(409,"이미 스크랩한 큐레이션입니다.");
 
     private final int code;
     private final String message;
