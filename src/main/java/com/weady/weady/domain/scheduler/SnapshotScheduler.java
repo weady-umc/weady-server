9dadc8c5f5769ba462f5c01b7c443f34bdf7ca55
jwpark <woojang812@gmail.com>
2025-08-12 01:31:14 +0900

    fix: 스케쥴러 시간 변경

--- src/main/java/com/weady/weady/domain/scheduler/SnapshotScheduler.java
+++ src/main/java/com/weady/weady/domain/scheduler/SnapshotScheduler.java
@@ -14,7 +14,7 @@ public class SnapshotScheduler {
     private final SnapshotService snapshotService;
 
     // 매일 23:30 KST에 내일(00~23시) 스냅샷 생성 + 지난 날짜 스냅샷 정리
-    @Scheduled(cron = "0 36 23 * * *", zone = "Asia/Seoul")
+    @Scheduled(cron = "0 27 23 * * *", zone = "Asia/Seoul")
     public void buildNextDaySnapshots() {
         log.info("[SnapshotScheduler] 내일 스냅샷 생성 시작");
         try {
